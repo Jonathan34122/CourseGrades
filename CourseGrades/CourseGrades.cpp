6a615b28961eec092d173e64f35d01252d3ce5ba
Jonathan <rockyroller2005@gmail.com>
2026-02-27 11:06:02 -0500

    Add project files.

---
+++ CourseGrades/CourseGrades.cpp
@@ -0,0 +1,17 @@
+// Purpose: Read the header values indicating the number of students and tests. Dynamically allocate an array of student structs. Dynamically allocate an array of test scores inside each struct. Store student name, ID, test scores, average, and letter grade.
+// Calculate average test scores. Assign letter grades using the provided grading scale. Produce a neatly formatted report and Deallocate memory in the correct order.
+
+// Author: Jonathan Lopez
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	
+
+
+
+
+}
