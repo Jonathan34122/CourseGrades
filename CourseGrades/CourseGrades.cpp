42364576c1ebcb57896337d6c90635e0b58dc10d
Jonathan <rockyroller2005@gmail.com>
2026-02-27 11:24:44 -0500

    * added functions and prototypes * added some comments for clarity

--- CourseGrades/CourseGrades.cpp
+++ CourseGrades/CourseGrades.cpp
@@ -1,16 +1,50 @@
 // Purpose: Read the header values indicating the number of students and tests. Dynamically allocate an array of student structs. Dynamically allocate an array of test scores inside each struct. Store student name, ID, test scores, average, and letter grade.
-// Calculate average test scores. Assign letter grades using the provided grading scale. Produce a neatly formatted report and Deallocate memory in the correct order.
+// Calculate average test scores. Assign letter grades using the provided grading scale and deallocate memory in the correct order.
 
 // Author: Jonathan Lopez
 
+
+
+// HEADER FILES
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+// PROTOTYPES
+void readData();
+void calculateAverages();
+void assignLetterGrades();
+void printReport();
 
 int main(){
 
 	
 
+
+
+
+}
+
+void readData() { // Purpose: to get data from student_data.txt
+
+	
+
+}
+
+void calculateAverages() { // calculate averages of each student's grades.
+
+
+
+}
+
+void assignLetterGrades() { // extract grades, assign them to a letter grade for each student.
+
+
+
+}
+
+void printReport() { // print final and complete report.
 
 
 
